d1aef94af50c226c8c8a485bb3a65195cb335d65
Jonathan M. Wilbur <jonathan@wilbur.space>
2020-12-29 12:14:34 -0500

    Add RDNSequenceFromString()
    Signed-off-by: Jonathan M. Wilbur <jonathan@wilbur.space>

---
+++ source/stringifiers/rdnSequenceFromString.ts
@@ -0,0 +1,160 @@
+import {
+    RelativeDistinguishedName as RDN,
+} from "../modules/InformationFramework/RelativeDistinguishedName.ta";
+import {
+    AttributeTypeAndValue as ATAV,
+} from "../modules/InformationFramework/AttributeTypeAndValue.ta";
+import {
+    OBJECT_IDENTIFIER,
+    ObjectIdentifier,
+    ASN1Element,
+    DERElement,
+} from "asn1-ts";
+import {
+    _encodePrintableString,
+    _encodeIA5String,
+} from "asn1-ts/dist/node/functional";
+import {
+    _encode_UnboundedDirectoryString as _encode_UBS,
+} from "../modules/SelectedAttributeTypes/UnboundedDirectoryString.ta";
+import * as selat from "../collections/selectedAttributeTypeAttributes";
+
+// OpenSSL on Windows 10:
+// Subject: CN = Jonathan Wilbur + dnQualifier = 68109699-a4f6-477e-a7b2-1246ef9717f3, ST = FL, C = US
+
+const encodeUBS = (str: string): ASN1Element => {
+    return _encode_UBS({
+        uTF8String: str,
+    }, () => new DERElement());
+};
+
+const encodePrintableString = (str: string): ASN1Element => {
+    return _encodePrintableString(str, () => new DERElement());
+};
+
+const encodeIA5String = (str: string): ASN1Element => {
+    return _encodeIA5String(str, () => new DERElement());
+};
+
+export
+const nameToAttribute = new Map<string, [ OBJECT_IDENTIFIER, (str: string) => ASN1Element ]>([
+    [ "cn", [ selat.commonName["&id"], encodeUBS ] ],
+    [ "gn", [ selat.givenName["&id"], encodeUBS ] ],
+    [ "sn", [ selat.surname["&id"], encodeUBS ] ],
+    [ "dnqualifier", [ selat.dnQualifier["&id"], encodePrintableString ] ],
+    [ "st", [ selat.stateOrProvinceName["&id"], encodeUBS ] ],
+    [ "c", [ selat.countryName["&id"], encodePrintableString ] ],
+    [ "o", [ selat.organizationName["&id"], encodeUBS ] ],
+    [ "ou", [ selat.organizationalUnitName["&id"], encodeUBS ] ],
+    [ "sn", [ selat.serialNumber["&id"], encodePrintableString ] ],
+    [ "initials", [ selat.initials["&id"], encodeUBS ] ],
+    [ "pseudonym", [ selat.pseudonym["&id"], encodeUBS ] ],
+    [ "generationqualifier", [ selat.generationQualifier["&id"], encodeUBS ] ],
+    [ "title", [ selat.title["&id"], encodeUBS ] ],
+
+    // This is not defined in any X.500 standard, but is still widely used. Defined in IETF RFC 2247.
+    [ "dc", [ new ObjectIdentifier([ 0, 9, 2342, 19200300, 100, 1, 25 ]), encodeIA5String ] ],
+]);
+
+const oidRegex = /^(0|1|2)\.\d+(?:\.\d+)+$/g;
+
+export
+function atavFromString (
+    str: string,
+    encodingMap: Map<string, [ OBJECT_IDENTIFIER, (str: string) => ASN1Element ]> = nameToAttribute,
+): ATAV {
+    const firstEqualSign: number = str.indexOf("=");
+    if (firstEqualSign === -1) {
+        throw new Error();
+    }
+    const name = str.slice(0, firstEqualSign).trim().toLowerCase();
+    const value = str.slice(firstEqualSign + 1).trim();
+    const attr = encodingMap.get(name);
+    if (attr) {
+        const [ oid, encoder ] = attr;
+        return new ATAV(
+            oid,
+            encoder(value),
+        );
+    } else {
+        if (oidRegex.test(name)) { // Then it is just an OID.
+            const oid: OBJECT_IDENTIFIER = new ObjectIdentifier(str.split(".").map((c) => Number.parseInt(c)));
+            return new ATAV(
+                oid,
+                encodeUBS(value), // We guess that it is a UBS...
+            );
+        } else {
+            throw new Error();
+        }
+    }
+}
+
+export
+function* rdnFromString (
+    str: string,
+    escape: number = "\\".charCodeAt(0),
+    atavDelimiter: number = "+".charCodeAt(0),
+    encodingMap: Map<string, [ OBJECT_IDENTIFIER, (str: string) => ASN1Element ]> = nameToAttribute,
+): IterableIterator<ATAV> {
+    let start: number = 0;
+    const escapeString: string = String.fromCharCode(escape);
+    for (let i: number = 0; i < str.length; i++) {
+        const char: number = str.charCodeAt(i);
+        const prevChar: number = str.charCodeAt(i - 1);
+        if ((char === atavDelimiter) && (prevChar !== escape)) {
+            const escapedATAVString = str
+                .slice(start, i)
+                .split(escapeString)
+                .map((segment: string): string => (segment.length) ? segment : escapeString)
+                .join("");
+            yield atavFromString(
+                escapedATAVString,
+                encodingMap,
+            );
+            i++;
+            start = i;
+        }
+    }
+    const escapedATAVString = str
+        .slice(start)
+        .split(escapeString)
+        .map((segment: string): string => (segment.length) ? segment : escapeString)
+        .join("");
+    yield atavFromString(
+        escapedATAVString,
+        encodingMap,
+    );
+}
+
+export default
+function* rdnSequenceFromString (
+    str: string,
+    escape: number = "\\".charCodeAt(0),
+    rdnDelimiter: number = ",".charCodeAt(0),
+    atavDelimiter: number = "+".charCodeAt(0),
+    encodingMap: Map<string, [ OBJECT_IDENTIFIER, (str: string) => ASN1Element ]> = nameToAttribute,
+): IterableIterator<RDN> {
+    let start: number = 0;
+    for (let i: number = 0; i < str.length; i++) {
+        const char: number = str.charCodeAt(i);
+        const prevChar: number = str.charCodeAt(i - 1);
+        if ((char === rdnDelimiter) && (prevChar !== escape)) {
+            const unescapedRDN = str.slice(start, i);
+            yield Array.from(rdnFromString(
+                unescapedRDN,
+                escape,
+                atavDelimiter,
+                encodingMap,
+            ));
+            i++;
+            start = i;
+        }
+    }
+    const unescapedRDN = str.slice(start);
+    yield Array.from(rdnFromString(
+        unescapedRDN,
+        escape,
+        atavDelimiter,
+        encodingMap,
+    ));
+}
--- test/jest/stringify.test.js
+++ test/jest/stringify.test.js
@@ -5,6 +5,14 @@ const { id_at_surname } = require("../../dist/node/modules/SelectedAttributeType
 const { id_at_organizationName } = require("../../dist/node/modules/SelectedAttributeTypes/id-at-organizationName.va");
 const rdnSequenceToString = require("../../dist/node/stringifiers/rdnSequenceToString").default;
 const generalNameToString = require("../../dist/node/stringifiers/generalNameToString").default;
+const rdnSequenceFromString = require("../../dist/node/stringifiers/rdnSequenceFromString").default;
+const decodeUBS = require("../../dist/node/modules/SelectedAttributeTypes/UnboundedDirectoryString.ta")
+    ._decode_UnboundedDirectoryString;
+const directoryStringToString = require("../../dist/node/stringifiers/directoryStringToString").default;
+
+// function getUBS (el) {
+//     return decodeUBS(el.sequence[0]);
+// }
 
 describe("Stringifiers", () => {
     const issuerCN = "Mr. Is/uer, Jr.";
@@ -46,5 +54,54 @@ describe("Stringifiers", () => {
 
     test("can stringify a GeneralName", () => {
         expect(generalNameToString(gn)).toBe("directoryName:gn=Mr. Is\\/uer, Jr.+sn=Mr. Is\\/uer, Jr./o=Mr. Is\\/uer, Jr.");
+    });
+
+    test("can convert a string to an RDNSequence", () => {
+        const str = "CN = Jonathan Wilbur + dnQualifier = 68109699-a4f6-477e-a7b2-1246ef9717f3, ST = FL, C = US";
+        const rdns = Array.from(rdnSequenceFromString(str));
+        expect(rdns.length).toBe(3);
+        expect(rdns[0].length).toBe(2);
+        expect(rdns[1].length).toBe(1);
+        expect(rdns[2].length).toBe(1);
+        expect(rdns[0][0].type_.toString()).toBe("2.5.4.3");
+        expect(rdns[0][1].type_.toString()).toBe("2.5.4.46");
+        expect(rdns[1][0].type_.toString()).toBe("2.5.4.8");
+        expect(rdns[2][0].type_.toString()).toBe("2.5.4.6");
+        expect(directoryStringToString(decodeUBS(rdns[0][0].value))).toBe("Jonathan Wilbur");
+        expect(rdns[0][1].value.printableString).toBe("68109699-a4f6-477e-a7b2-1246ef9717f3");
+        expect(directoryStringToString(decodeUBS(rdns[1][0].value))).toBe("FL");
+        expect(directoryStringToString(decodeUBS(rdns[2][0].value))).toBe("US");
+    });
+
+    test("can convert a string containing escapes to an RDNSequence", () => {
+        const str = "CN = Wilbur\\, Jonathan + dnQualifier = bigboi\\+badboi, ST = FL, C = US";
+        const rdns = Array.from(rdnSequenceFromString(str));
+        expect(rdns.length).toBe(3);
+        expect(rdns[0].length).toBe(2);
+        expect(rdns[1].length).toBe(1);
+        expect(rdns[2].length).toBe(1);
+        expect(rdns[0][0].type_.toString()).toBe("2.5.4.3");
+        expect(rdns[0][1].type_.toString()).toBe("2.5.4.46");
+        expect(rdns[1][0].type_.toString()).toBe("2.5.4.8");
+        expect(rdns[2][0].type_.toString()).toBe("2.5.4.6");
+        expect(directoryStringToString(decodeUBS(rdns[0][0].value))).toBe("Wilbur, Jonathan");
+        expect(rdns[0][1].value.printableString).toBe("bigboi+badboi");
+        expect(directoryStringToString(decodeUBS(rdns[1][0].value))).toBe("FL");
+        expect(directoryStringToString(decodeUBS(rdns[2][0].value))).toBe("US");
+    });
+
+    test("can convert a string containing escaped escapes to an RDNSequence", () => {
+        const str = "CN = Jonathan \\\\Wilbur, ST = FL, C = US";
+        const rdns = Array.from(rdnSequenceFromString(str));
+        expect(rdns.length).toBe(3);
+        expect(rdns[0].length).toBe(1);
+        expect(rdns[1].length).toBe(1);
+        expect(rdns[2].length).toBe(1);
+        expect(rdns[0][0].type_.toString()).toBe("2.5.4.3");
+        expect(rdns[1][0].type_.toString()).toBe("2.5.4.8");
+        expect(rdns[2][0].type_.toString()).toBe("2.5.4.6");
+        expect(directoryStringToString(decodeUBS(rdns[0][0].value))).toBe("Jonathan \\Wilbur");
+        expect(directoryStringToString(decodeUBS(rdns[1][0].value))).toBe("FL");
+        expect(directoryStringToString(decodeUBS(rdns[2][0].value))).toBe("US");
     });
 });
