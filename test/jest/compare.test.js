c45b6d290d88679012ceb77dbde698741f912cea
Jonathan M. Wilbur <jonathan@wilbur.space>
2020-12-29 19:32:56 -0500

    Fix test bug
    Signed-off-by: Jonathan M. Wilbur <jonathan@wilbur.space>

--- test/jest/compare.test.js
+++ test/jest/compare.test.js
@@ -1,7 +1,7 @@
 const asn1 = require("asn1-ts");
 const { AttributeTypeAndValue } = require("../../dist/node/modules/InformationFramework/AttributeTypeAndValue.ta");
 const { id_at_givenName } = require("../../dist/node/modules/SelectedAttributeTypes/id-at-givenName.va");
-const { id_at_surname } = require("../../dist/node/modules/SelectedAttributeTypes/id-at-surName.va");
+const { id_at_surname } = require("../../dist/node/modules/SelectedAttributeTypes/id-at-surname.va");
 const { id_at_organizationName } = require("../../dist/node/modules/SelectedAttributeTypes/id-at-organizationName.va");
 const compareRDNSequence = require("../../dist/node/comparators/compareRDNSequence").default;
 
